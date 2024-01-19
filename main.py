64d2be445572f6f2132fedf814d4e959ae8f676e
ys <ys@sina.com>
2024-01-19 17:22:16 +0800

    限制IP

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, request, abort
 from flask_cors import *
 
 from controller.TextSimController import similarity_route
 from controller.TrainController import train_route
 
-
-
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+# 定义允许访问的IP白名单
+allowed_ips = ['127.0.0.1', '192.168.1.4']  # '127.0.0.1', '192.168.1.1'
+
+
+# 在每个请求之前执行的拦截器
+@app.before_request
+def before_request():
+    client_ip = request.remote_addr
+    print("request.remote_addr: " + request.remote_addr)
+    if client_ip not in allowed_ips:
+        abort(403)
 
 
 app.register_blueprint(train_route)
